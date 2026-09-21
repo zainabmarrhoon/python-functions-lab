c5438dd73f99e2748b325ee0fb1ccd5ee7550ed1
zainab-16-marker <zaynp01602@gmail.com>
2026-09-21 09:43:48 +0300

    fiish ex 4-5-6

--- exercises.py
+++ exercises.py
@@ -54,3 +54,82 @@ def apply_discount(product,percentage):
 
 
 print('Exercise 3:', apply_discount(100, 25))
+
+
+
+
+# Exercise 4: Convert Temperature
+#
+# Write a function called `convert_temperature` that takes a
+# temperature and a unit ('C' for Celsius, 'F' for Fahrenheit)
+# and converts the temperature to the other unit.
+# The formula for converting Celsius to Fahrenheit is (Celsius * 9/5) + 32.
+# The formula for converting Fahrenheit to Celsius is (Fahrenheit - 32) * 5/9.
+#
+# Examples:
+# convert_temperature(0, 'C') should return 32.0.
+# convert_temperature(32, 'F') should return 0.0.
+#
+# Define the function and then call it below.
+
+def convert_temperature(number,unit):
+    if(unit=="F"):
+        return (number * 9 / 5) + 32
+    else:
+        return (number - 32) * 5 / 9
+
+
+print('Exercise 4: Convert 0°C to Fahrenheit:', convert_temperature(0, 'C'))
+print('Exercise 4: Convert 32°F to Celsius:', convert_temperature(32, 'F'))
+
+
+
+
+
+
+# Exercise 5: Sum to N
+#
+# Write a function named `sum_to` that takes a single integer n and returns the sum of all integers from 1 to n.
+#
+# Examples:
+# sum_to(6) should return 21.
+# sum_to(10) should return 55.
+#
+# Define the function and then call it below.
+
+def sum_to(n):
+    total = 0
+
+    for number in range(1,n+1):
+        total += number
+
+        return total
+
+
+print('Exercise 5:', sum_to(6))
+
+
+
+
+
+
+# Exercise 6: Find the Largest Number
+#
+# Write a function named `largest` that takes three integers as arguments and returns the largest of them.
+#
+# Examples:
+# largest(1, 2, 3) should return 3.
+# largest(10, 4, 2) should return 10.
+#
+# Define your function and test it with different inputs.
+
+def largest(num1,num2,num3):
+     if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3 
+
+
+print('Exercise 6:', largest(1, 2, 3))
